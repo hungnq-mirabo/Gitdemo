6dbc166085ae4af59a60c3cc72c92a1c117d46b2
hungnq-mirabo <hungnq@mirabo.com.vn>
2022-01-08 14:50:08 +0700

    Up code of practice-2

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            myName: 'Nguyen Quoc Hung',
-            myAge: 32,
-            imgLink: 'https://www.udemy.com/staticx/udemy/images/v7/logo-udemy-inverted.svg'
-        };
+  data() {
+    return {
+      counter: 10,
+      num: 10,
+      name: '',
+      confirmName: '',
+      alertMsg: 'This is an alert message!'
+    };
+  },
+  methods: {
+    confirmInput(event) {
+      this.confirmName = event.target.value;
     },
-
-    methods: {
-        myAgeAfter5Year() {
-            return this.myAge + 5;
-        }
+    showAlert() {
+      alert(this.alertMsg);
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
     }
+  }
 });
 
-app.mount('#user-goal');
+app.mount('#events');
